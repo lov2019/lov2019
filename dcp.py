ab2ba56543bd81265e27a380c9f4ea07ee3f1cf9
lov2019 <155280798+lov2019@users.noreply.github.com>
2024-01-01 12:35:02 +0800

    Update dcp.py

--- dcp.py
+++ dcp.py
@@ -1,1 +1,22 @@
 from rich import console
+import os
+rc=True
+con=console.Console()
+def r():
+    if rc:
+        rc=False
+    else:
+        rc=True
+    return
+def x():
+    exit(0)
+    return
+def c():
+    try:
+        os.chdir(input("chdir>"))
+    except:
+        con.print("Can not change work dir!",style="bold red")
+    else:
+        con.print("Change work dir finish!",style="bold green")
+    return
+        
