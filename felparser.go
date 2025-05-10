3db9f687a8277394d23e83af8939c8ba2cd41794
lov2019 <155280798+lov2019@users.noreply.github.com>
2025-05-10 12:23:19 +0800

    Update felparser.go

--- felparser.go
+++ felparser.go
@@ -1,6 +1,19 @@
+package femlparser
+
 import (
     "fmt"
-    "os"
     "strings"
 )
-func WriteToFile(data map
+
+func MapToFEML(src map[string]string) string {
+    rc := ""
+    for k, v := range src {
+        rc += fmt.Sprintf("%s=%s\n", k, v)
+        }
+    return rc
+}
+
+func ParseFEML(src string) map[string]string {
+    rc := make(map[string]string)
+    ei := 0
+    
