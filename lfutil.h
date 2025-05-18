ca97ac3c3133337032daacc01c79d94ef15cebb6
lov2019 <155280798+lov2019@users.noreply.github.com>
2025-05-18 14:13:51 +0800

    Update lfutil.h

--- lfutil.h
+++ lfutil.h
@@ -1,5 +1,6 @@
 #ifndef LFUTIL_H
-#defind LFUTIL_H
+#define LFUTIL_H
+#define NO_TEST // 删除这行代码以在编译中包含测试
 #include <string.h>
 int lfstr_indexOf(char *src, char *need) {
     if (*need == '\0') {
@@ -11,4 +12,8 @@ int lfstr_indexOf(char *src, char *need) {
 bool lfstr_equals(char *a, char *b) {
     return !strcmp(a, b);
 }
+#ifndef NO_TEST
+void test() {
+}
+#endif
 #endif
