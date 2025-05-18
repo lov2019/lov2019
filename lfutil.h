a149cf715adf9f63453f261cdaf4b4d2da7a63a5
lov2019 <155280798+lov2019@users.noreply.github.com>
2025-05-18 10:35:52 +0800

    Update lfutil.h

--- lfutil.h
+++ lfutil.h
@@ -1,3 +1,11 @@
 #ifndef LFUTIL_H
 #defind LFUTIL_H
+#include <string.h>
+int lfstr_indexOf(char *src, char *need) {
+    if (*need == '\0') {
+        return -1:
+    }
+    char *res = strstr(src, need);
+    return res ? res - src : NULL;
+}
 #endif
