0cfa178e3591bc971f39be58f9c540a83bc1763f
lov2019 <155280798+lov2019@users.noreply.github.com>
2025-06-22 05:25:58 +0800

    Update lfstr.h

--- lfstr.h
+++ lfstr.h
@@ -1,10 +1,25 @@
 #ifndef LFSTR_H
 #define LFSTR_H
 #include <string.h>
+#include <stdlib.h>
 struct LFSTR {
     char *data;
     int size;
 };
-bool lfstrHinit(LFSTR *s) {
+
+bool lfstrHinit(LFSTR *s, int size = 16) {
+    s->data = (char *)malloc(sizeof(char) * size);
+    if (s->data == NULL) {
+        return false;
+    }
+    s->size = size;
+    return true;
+}
+
+void lfstrHfree(LFSTR *s) {
+    free(s->data);
+    s->data = NULL;
+    s->size = 0;
+}
     
 #endif
