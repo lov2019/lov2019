94cd90340b765d26356c7a47dbc242387c647f6c
lov2019 <155280798+lov2019@users.noreply.github.com>
2025-07-20 04:45:34 +0800

    Update main.c

--- DevC++官方案例/文本编辑器/main.c
+++ DevC++官方案例/文本编辑器/main.c
@@ -1,5 +1,11 @@
 #include <windows.h>
-#include "main.h"
+
+#define CM_FILE_SAVEAS	9072
+#define CM_FILE_EXIT	9071
+#define CM_FILE_OPEN	9070
+#define CM_ABOUT        9069
+
+#define IDC_MAIN_TEXT 1001
 
 BOOL LoadFile(HWND hEdit, LPSTR pszFileName) {
 	HANDLE hFile;
